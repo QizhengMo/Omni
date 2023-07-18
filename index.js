0d2d214e5517eb4c7d1fefaca5223539beab044f
QizhengMo <QizhengMo@users.noreply.github.com>
2023-07-18 14:10:30 +0000

    deploy: 0c2fbadb2b0ff5030f4559709aa4fc7ee4624a49

---
+++ index.js
@@ -0,0 +1,1 @@
+import"./assets/_commonjsHelpers-a1841df3.js";import{a as o,j as t,R as e}from"./assets/client-b54b429a.js";import{P as r}from"./assets/Panel-b27b9825.js";const n=o.createRoot(document.getElementById("root"));n.render(t.jsx(e.StrictMode,{children:t.jsx(r,{})}));
---
+++ src/pages/background/index.js
@@ -0,0 +1,1 @@
+function e(){console.log("background script loaded"),chrome.sidePanel.setPanelBehavior({openPanelOnActionClick:!0})}e();
