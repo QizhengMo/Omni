b7cf5392a0a6d8bc71c0727621aaf632318b79d0
QizhengMo <QizhengMo@users.noreply.github.com>
2023-07-19 14:47:22 +0000

    deploy: 51c986599e2e4d47a3618bb59aaa003e5e76deab

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-import"./assets/_commonjsHelpers-a1841df3.js";import{a as o,j as t,R as e}from"./assets/client-b54b429a.js";import{P as r}from"./assets/Panel-e191e7de.js";const n=o.createRoot(document.getElementById("root"));n.render(t.jsx(e.StrictMode,{children:t.jsx(r,{})}));
+import"./assets/_commonjsHelpers-a1841df3.js";import{a as o,j as t,R as e}from"./assets/client-b54b429a.js";import{P as r}from"./assets/Panel-9c4e8d3d.js";const n=o.createRoot(document.getElementById("root"));n.render(t.jsx(e.StrictMode,{children:t.jsx(r,{})}));
