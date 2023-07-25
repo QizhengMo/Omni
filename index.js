fb5fdb87b46cc6a9aabb59cadef080f7b73d3152
QizhengMo <QizhengMo@users.noreply.github.com>
2023-07-25 05:46:46 +0000

    deploy: f929f97e33cfd532bbb8b54e81b5408795205488

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-import"./assets/_commonjsHelpers-a1841df3.js";import{a as o,j as t,R as e}from"./assets/client-b54b429a.js";import{P as r}from"./assets/Panel-a911e5e6.js";const n=o.createRoot(document.getElementById("root"));n.render(t.jsx(e.StrictMode,{children:t.jsx(r,{})}));
+import"./assets/_commonjsHelpers-a1841df3.js";import{a as o,j as t,R as e}from"./assets/client-b54b429a.js";import{t as n,P as r}from"./assets/Panel-040afd31.js";const s=o.createRoot(document.getElementById("root"));n().then(()=>{console.log("ZstdInit done"),s.render(t.jsx(e.StrictMode,{children:t.jsx(r,{})}))});
