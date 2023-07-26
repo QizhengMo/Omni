c099a8458658fd38f1f734690205b674c9bedc75
QizhengMo <moqizhengz@gmail.com>
2023-07-26 23:14:14 +0800

    refactor: header select

--- src/pages/panel/Header.tsx
+++ src/pages/panel/Header.tsx
@@ -1,27 +1,48 @@
 import * as React from "react";
 import { ModuleContext, MODULES, Module } from "@pages/panel/Panel";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 type Props = { setSelected: (module: Module) => void };
+
 export function Header(props: Props) {
   const { setSelected } = props;
   const selected = useContext(ModuleContext);
+  const [open, setOpen] = useState(false);
   return (
-    <div className="navbar bg-base-100 pr-6">
+    <div className="navbar bg-base-100 pr-6 h-12 flex items-start">
       <div className="navbar-start">
         <a className="btn btn-ghost normal-case text-xl">Omni</a>
       </div>
 
-      <div className="navbar-end lg:flex">
-        <ul className="menu menu-horizontal px-1 bg-base-200">
-          {Object.values(MODULES).map((module) => (
-            <HeaderItem
-              key={JSON.stringify(module)}
-              selected={selected}
-              module={module}
-              onClick={() => setSelected(module)}
-            />
-          ))}
+      <div className="navbar-end lg:flex ">
+        <ul className="menu lg:menu-horizontal bg-base-200 rounded-box lg:mb-64">
+          <li>
+            <span
+              onClick={() => {
+                setOpen((old) => !old);
+              }}
+              className={
+                "menu-dropdown-toggle" + (open ? "menu-dropdown-show" : "")
+              }
+            >
+              {selected.displayName}
+            </span>
+            <ul
+              className={"menu-dropdown" + (open ? "menu-dropdown-show" : "")}
+            >
+              {Object.values(MODULES).map((module) => (
+                <HeaderItem
+                  key={JSON.stringify(module)}
+                  selected={selected}
+                  module={module}
+                  onClick={() => {
+                    setOpen(false)
+                    setSelected(module);
+                  }}
+                />
+              ))}
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
@@ -35,13 +56,10 @@ type HeaderItemProps = {
 };
 const HeaderItem = (props: HeaderItemProps) => {
   const { module, onClick, selected } = props;
-  const selfSelected = selected === module;
   const moduleDisName = Object.values(module)[0];
   return (
     <li>
-      <a onClick={onClick} className={selfSelected ? "active" : ""}>
-        {moduleDisName}
-      </a>
+      <a onClick={onClick}>{moduleDisName}</a>
     </li>
   );
 };
